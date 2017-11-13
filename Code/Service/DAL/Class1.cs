430492e1ec2a258be10706a60489af7ba62d73cb
Jonas Laursen <jona8690@edu.eal.dk>
2017-11-13 14:33:37 +0100

    Created DAL Again #33

---
+++ Code/Service/DAL/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Class1
+    {
+        public void item()
+        {
+            LaerDanskDataSet stuff = new LaerDanskDataSet();
+
+            stuff.GetChanges();
+        }
+    }
+}
