ba923bfe4a15e503f0396d1794cfded4d5336e2d
Jonas Laursen <jona8690@edu.eal.dk>
2017-11-22 13:16:50 +0100

    Removed unsessesarry usings

--- Code/Service/Service/Controllers/LecturePlanController.cs
+++ Code/Service/Service/Controllers/LecturePlanController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
-using System.Linq;
 using Core;
 using System.Web.Script.Serialization;
 
