d969bdf0f844a97a925252793caea5ebd3e9e03e
Jonas Laursen <jona8690@edu.eal.dk>
2017-11-27 12:48:58 +0100

    Created unit test class for UserRepoTests

---
+++ Code/Service/Service.Tests/UserRepositoryUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Service.Tests
+{
+    /// <summary>
+    /// Summary description for UserRepositoryUnitTests
+    /// </summary>
+    [TestClass]
+    public class UserRepositoryUnitTests
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
